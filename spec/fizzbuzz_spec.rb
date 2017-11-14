28dde5a67f5db11022806ae21c42310b51d828f0
Nikolaos Sakellarios <nikolaos@sakellarios.net>
2017-11-14 14:45:36 +0000

    Test 2: divided by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
 require "fizzbuzz"
 
 describe 'fizzbuzz' do
+
   it 'returns "fizz" when passed 3' do
     expect(3.fizzbuzz).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+
 end
